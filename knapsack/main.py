18b8a5371e930f55b963a8a90ac86a142f8e4f00
ftrihardjo <felixtrihardjo@gmail.com>
2024-02-06 09:34:53 +0700

    solver

--- knapsack/main.py
+++ knapsack/main.py
@@ -5,5 +5,5 @@ from os import listdir
 if __name__ == '__main__':
     solution = []
     for i in listdir('data'):
-        solution.append({'file':i,'solution':solve(i)})
+        solution.append({'file':i,'solution':solve(f'data/{i}')})
     print(solution)
